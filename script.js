93f84a51a1c21bc6c0f938c8c1cb3300b17e6fab
Adonay Mendez <adonay.mendez255@gmail.com>
2026-07-17 14:45:33 -0400

    Update styling and functionality

--- script.js
+++ script.js
@@ -13,9 +13,22 @@ const handleClick = (e) => {
   let type = e.target.textContent.toLowerCase();
 
   updateCards(type);
+
+  updateActiveButton(e.target);
 };
 
 const updateCards = (type) => {
+
+  let label;
+ 
+  if (type === "daily"){
+     label = 'Yesterday';
+  } else if (type === "weekly"){
+     label = 'Last Week'
+  } else {
+      label = 'Last Month'
+  }
+
   // card = current element(div) : index = current position
   cards.forEach((card, index) => {
     // make item variable have access to progressData[current position]
@@ -25,9 +38,19 @@ const updateCards = (type) => {
     // Get the elements inside the div and change their textContent accordingly
     // [type] retrived from handleClick function (daily, weekly, monthly)
     card.querySelector("h3").textContent = item.title;
-    card.querySelector(".current").textContent = `${item.timeframes[type].current}hrs`;
-    card.querySelector(".previous").textContent = `Last Week - ${item.timeframes[type].previous}hrs`;
+    card.querySelector(".current").textContent =
+      `${item.timeframes[type].current}hrs`;
+    card.querySelector(".previous").textContent =
+      `${label} - ${item.timeframes[type].previous}hrs`;
   });
+};
+
+const updateActiveButton = (button) => {
+  buttons.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  button.classList.add("active");
 };
 
 buttons.forEach((button) => {
@@ -45,6 +68,7 @@ fetch("data.json")
   .then((data) => {
     progressData = data;
 
-    // Show weekly by default 
-    updateCards('weekly');
+    // Show weekly by default
+    updateCards("weekly");
+    updateActiveButton(weeklyBtn);
   });
